96fc927ab472cc1399b725d8690ee6630cc8970d
Pitipat Srichairat <pitipat.dop@gmail.com>
2017-07-26 12:32:10 +0700

    test travis sql

--- test/index.test.js
+++ test/index.test.js
@@ -2,6 +2,7 @@
 process.env.NODE_ENV = 'test';
 
 // Require the dev-dependencies
+const sinon = require('sinon');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../app');
@@ -10,15 +11,55 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 
-// Our parent block
-describe('Realtime', () => {
+describe('/stops', () => {
 
-  /*
-   * Test the /GET route
-   */
-  describe('vehicle', () => {
-    it('should return vehicle data', (done) => {
+});
 
+describe('/realtime', () => {
+
+  describe('/trips', () => {
+    it('should return 400 if no parameter given', (done) => {
+      chai.request(server)
+        .get('/v1/realtime/trips')
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+
+    it('should return 200 to success request', (done) => {
+      chai.request(server)
+        .get('/v1/realtime/trips/?arriving-at=7881')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('data');
+          done();
+        });
+    });
+
+    it('should return 200 to success request', (done) => {
+      chai.request(server)
+        .get('/v1/realtime/trips/?arriving-at=7881&route-id=380')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('data');
+          done();
+        });
+    });
+  });
+
+  describe('/vehicles', () => {
+
+    it('should return 404 to /v1/realtime/vehicle', (done) => {
+      chai.request(server)
+        .get('/v1/realtime/vehicles')
+        .end((err, res) => {
+          res.should.have.status(404);
+          done();
+        });
+    });
+
+    it('should return 200 to success request', (done) => {
       chai.request(server)
         .get('/v1/realtime/vehicles/380')
         .end((err, res) => {
